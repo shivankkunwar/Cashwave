4e2b79c5bf42dbce1998f249a4f81d5dc757f53f
shivank <shivankkunwar100@gmail.com>
2024-02-29 00:15:31 +0530

    coded signIn signUp API

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = 3000
-const userRouter = require("./routes/index")
+const v1Router = require("./routes/index")
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.use("/app/v1",userRouter);
+app.use("/api/v1",v1Router);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,77 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+const { User } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+// signIn signUp
+const signUpSchema = zod.object({
+  userName: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const { success } = signUpSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+  const userExist = await User.findOne({
+    userName: req.body.userName,
+  });
+  if (userExist) {
+    return res.status(411).json({
+      message: "Email already taken / Incorrect inputs",
+    });
+  }
+
+  const dbUser = await User.create(body);
+  const userId = dbUser._id;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+  res.json({
+    message: "User created successfully",
+    token: token,
+  });
+});
+const signInBody = zod.object({
+  userName: zod.string().email(),
+  password: zod.string(),
+});
+router.post("/signin", async (req, res) => {
+  const { success } = signInBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+  const userExist = await User.findOne({
+    userName: req.body.userName,
+    password: req.body.password,
+  });
+
+  if (userExist) {
+    const userId = userExist._id;
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      JWT_SECRET
+    );
+    res.status(200).json({
+      token: token,
+    });
+    return;
+  }
+  res.status(411).json({
+    message: "Error while logging in",
+  });
+});
 module.exports = router;
